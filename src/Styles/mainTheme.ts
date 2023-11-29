5ba8d15f13162bc018398eef3c1911c992a7a716
ramiro elisei <ramiroelisei@gmail.com>
2023-11-29 12:56:11 +0100

    Add mainTheme

---
+++ src/Styles/mainTheme.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  color: {
+    main: "#3a3b3b",
+    secondary: "#f6f1e4",
+    accentBright: "#11a84b",
+    accentMedium: "#1979be",
+    backgroundLight: "#d7a2ca",
+    backgorundMedium: "#d9dbd1",
+    backgroundDark: "#06604d",
+    disable: "#e5e3d8",
+    successfull: "#0e8a70",
+    unSuccessfull: "#c32b3d",
+  },
+
+  typography: {
+    mainFont: "'IBM Plex Sans'  sans-serif",
+  },
+};
+
+export default mainTheme;
