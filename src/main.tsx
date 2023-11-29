71b18ed336f95c246d63a8f3b8975bbafdfcd27e
ramiro elisei <ramiroelisei@gmail.com>
2023-11-29 19:29:11 +0100

    Add Header test

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./Styles/mainTheme";
+import GlobalStyle from "./Styles/GlobalStyle";
+import App from "./components/App/App";
+import "@fontsource/ibm-plex-sans";
+import "@fontsource/ibm-plex-sans/400.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
-  </React.StrictMode>
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
