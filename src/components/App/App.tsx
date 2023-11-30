9ff4768b85ca1aa46776ad4b018590e7cf2cb534
ramiro elisei <ramiroelisei@gmail.com>
2023-11-30 18:15:00 +0100

    Add activity type

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
+import ActivitiesList from "../ActivitiesList/ActivitiesList";
+import activitiesMockData from "../../mocks/activitiesMock";
 
 const App = (): React.ReactElement => {
   return (
@@ -7,6 +9,10 @@ const App = (): React.ReactElement => {
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
+        <Route
+          path="/home"
+          element={<ActivitiesList activities={activitiesMockData} />}
+        />
       </Routes>
     </>
   );
