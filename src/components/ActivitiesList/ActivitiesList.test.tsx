aacc38559727517409c9f1a26b98e4a6eec4f109
ramiro elisei <ramiroelisei@gmail.com>
2023-11-30 18:12:35 +0100

    Add ActivitiesList test

---
+++ src/components/ActivitiesList/ActivitiesList.test.tsx
@@ -0,0 +1,30 @@
+import { screen } from "@testing-library/react";
+import customRenderProvider from "../../utils/customRenderProvider";
+import ActivitiesList from "./ActivitiesList";
+import activitiesMockData from "../../mocks/activitiesMock";
+
+describe("Given an ActivitiesList component", () => {
+  describe("When the MechasList it is render", () => {
+    test("Then it should show as first position the heading 'Generative plant gadering'", () => {
+      const expectedActivityName = "Generative plant gadering";
+
+      customRenderProvider(<ActivitiesList activities={activitiesMockData} />);
+
+      const activityName = screen.getByRole("heading", {
+        name: expectedActivityName,
+      });
+
+      expect(activityName).toBeInTheDocument();
+    });
+
+    test("Then should appear as a second position the alternative picture name 'Absurdity Unleashed Talk'", () => {
+      const expectedAltText = activitiesMockData[1].activityName;
+
+      customRenderProvider(<ActivitiesList activities={activitiesMockData} />);
+
+      const imageAltText = screen.getByAltText(expectedAltText);
+
+      expect(imageAltText).toBeInTheDocument();
+    });
+  });
+});
