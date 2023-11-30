94e30febd50506abf6e14fbfdae45087b7291de5
ramiro elisei <ramiroelisei@gmail.com>
2023-11-30 18:13:41 +0100

    Add activities mock

---
+++ src/mocks/activitiesMock.ts
@@ -0,0 +1,28 @@
+import { ActivityStructure } from "../store/features/activities/types";
+
+const activitiesMockData: ActivityStructure[] = [
+  {
+    _id: "656493fa44b5521c2584c216",
+    activityType: "Workshop",
+    activityName: "Generative plant gadering",
+    ledBy: "Steve Green and Lia Rasmussen",
+    date: "",
+    startTime: "15:00",
+    activityDescription:
+      "Cultivate sustainable ideas in our playful Generative Plant Gathering workshop: where creativity blossoms with environmental consciousness.",
+    imageUrl: "https://i.ibb.co/8zFCCQk/plant-gadering.webp",
+  },
+  {
+    _id: "656493fa44b5521c2584c21c",
+    activityType: "Talks",
+    activityName: "Absurdity Unleashed Talk",
+    ledBy: "Jester Jack",
+    date: "",
+    startTime: "11:45",
+    activityDescription:
+      "Jack humorously explores wildly creative yet absurd solutions to sustainable challenges in engaging, thought-provoking lectures.",
+    imageUrl: "https://i.ibb.co/0jgqppJ/Absurdity-Talk.webp",
+  },
+];
+
+export default activitiesMockData;
