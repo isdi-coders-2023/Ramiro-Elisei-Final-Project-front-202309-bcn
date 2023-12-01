48395fdd51891dcb674ae3c2487e3d2d93ad53af
ramiro elisei <ramiroelisei@gmail.com>
2023-12-01 09:10:35 +0100

    Update custom render with store

--- src/utils/customRenderProvider.tsx
+++ src/utils/customRenderProvider.tsx
@@ -3,12 +3,22 @@ import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import mainTheme from "../Styles/mainTheme";
 import GlobalStyle from "../Styles/GlobalStyle";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { activitiesReducer } from "../store/features/activities/activitiesSlice";
+import activitiesMockData from "../mocks/activitiesMock";
 
 const customRenderProvider = (children: React.ReactElement) => {
+  const mockStore = configureStore({
+    reducer: { activitiesState: activitiesReducer },
+    preloadedState: { activitiesState: { activities: activitiesMockData } },
+  });
   return render(
     <BrowserRouter>
       <ThemeProvider theme={mainTheme}>{children}</ThemeProvider>
-      <GlobalStyle />
+      <Provider store={mockStore}>
+        <GlobalStyle />
+      </Provider>
     </BrowserRouter>,
   );
 };
