05509945d15571b29a3763cc9b9d0ee21d4e2f88
ramiro elisei <ramiroelisei@gmail.com>
2023-12-01 16:41:56 +0100

    Add load activities

--- src/pages/ActivitiesPage/ActivitiesPage.tsx
+++ src/pages/ActivitiesPage/ActivitiesPage.tsx
@@ -1,8 +1,22 @@
+import ActivitiesPageStyle from "./ActivitiesPageStyle";
 import ActivitiesList from "../../components/ActivitiesList/ActivitiesList";
 import activitiesMockData from "../../mocks/activitiesMock";
-import ActivitiesPageStyle from "./ActivitiesPageStyle";
+import { useEffect } from "react";
+import { loadActivitiesActionCreator } from "../../store/features/activities/activitiesSlice";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 
 const ActivitiesPage = (): React.ReactElement => {
+  const activitiesState = useAppSelector(
+    (activity) => activity.activitiesState,
+  );
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      dispatch(loadActivitiesActionCreator(activitiesMockData));
+    })();
+  }, [dispatch]);
+
   return (
     <>
       <ActivitiesPageStyle>
@@ -10,7 +24,7 @@ const ActivitiesPage = (): React.ReactElement => {
         <span className="page__date">dic 27 - 26</span>
         <span className="page__date">2023</span>
       </ActivitiesPageStyle>
-      <ActivitiesList activities={activitiesMockData} />
+      <ActivitiesList activities={activitiesState.activities} />
     </>
   );
 };
