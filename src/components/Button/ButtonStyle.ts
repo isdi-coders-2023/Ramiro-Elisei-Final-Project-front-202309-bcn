fd1912c5088dfb7e72cc41742544127894b8b2cb
ramiro elisei <ramiroelisei@gmail.com>
2023-12-02 16:32:35 +0100

    Add button styles

---
+++ src/components/Button/ButtonStyle.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const ButtonStyle = styled.button`
+  border-radius: 50px;
+  text-align: center;
+
+  &.button--small {
+    width: 45%;
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: 400;
+  }
+
+  &.button--solid {
+    background-color: ${({ theme }) => theme.color.main};
+    color: ${({ theme }) => theme.color.secondary};
+    padding: 12px 40px;
+  }
+
+  &.button--dotted {
+    border: 2.5px dashed ${({ theme }) => theme.color.main};
+    color: ${({ theme }) => theme.color.main};
+    padding: 9px 37px;
+  }
+`;
+
+export default ButtonStyle;
