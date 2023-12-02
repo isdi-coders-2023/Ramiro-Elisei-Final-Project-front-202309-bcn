2cdee45aad2e1f903b9237a1328df3c5d754c5da
ramiro elisei <ramiroelisei@gmail.com>
2023-12-02 16:34:54 +0100

    Add Button component to ActivityCard

--- src/components/ActivityCard/ActivityCardStyle.ts
+++ src/components/ActivityCard/ActivityCardStyle.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 const ActivityCardStyled = styled.article`
+  display: flex;
+  flex-direction: column;
+
   .activity-card {
     &__panel {
       margin: 18px 0 9px 0;
@@ -19,6 +22,12 @@ const ActivityCardStyled = styled.article`
 
     &__led {
       font-size: 16px;
+      margin-bottom: 25px;
+    }
+
+    &__toolbar {
+      display: flex;
+      justify-content: space-between;
     }
   }
 `;
