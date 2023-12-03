40a38ecb47a1ede76fcfe7223e83a4afd0337d74
ramiro elisei <ramiroelisei@gmail.com>
2023-12-03 15:25:09 +0100

    Add custom render second parameter in Navigation

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,13 +1,15 @@
 import { screen } from "@testing-library/react";
 import Navigation from "./Navigation";
 import customRenderProvider from "../../utils/customRenderProvider";
+import activitiesMockData from "../../mocks/activitiesMock";
 
 describe("Given a Navigation component", () => {
   describe("When it is rendered", () => {
     test("It should show a 'Home' link", () => {
       const expetedLinkText = "Home";
+      const mockData = activitiesMockData;
 
-      customRenderProvider(<Navigation />);
+      customRenderProvider(<Navigation />, mockData);
 
       const navigationHomeLink = screen.getByRole("link", {
         name: expetedLinkText,
