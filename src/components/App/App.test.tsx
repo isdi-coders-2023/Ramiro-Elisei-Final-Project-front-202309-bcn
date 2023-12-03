c16a28bca34f4df60da3dba2b8fe74c6a8746f9f
ramiro elisei <ramiroelisei@gmail.com>
2023-12-03 17:08:07 +0100

    Add App test

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,42 @@
+import { screen } from "@testing-library/react";
+import App from "./App";
+import customRenderProvider, {
+  customRenderWithoutBrowserRouter,
+} from "../../utils/customRenderProvider";
+import activitiesMockData from "../../mocks/activitiesMock";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Given an App component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show 'ludic' logo in the header", () => {
+      const expectedLogoAltName = "Ludic logo";
+      const mockData = activitiesMockData;
+
+      customRenderProvider(<App />, mockData);
+
+      const logo = screen.getByRole("img", {
+        name: expectedLogoAltName,
+      });
+
+      expect(logo).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered with a home path", () => {
+    test("Then it should show the text 'Activities' in a heading", () => {
+      const expectedHeadingText = "activities";
+
+      customRenderWithoutBrowserRouter(
+        <MemoryRouter initialEntries={["/home"]}>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const heading = screen.getByRole("heading", {
+        name: expectedHeadingText,
+      });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
