9e6c66b6db8e77707becd80528815c02873c141b
ramiro elisei <ramiroelisei@gmail.com>
2023-12-03 15:24:31 +0100

    Add custom render second parameter in Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,12 +1,15 @@
 import { fireEvent, screen } from "@testing-library/react";
 import customRenderProvider from "../../utils/customRenderProvider";
 import Button from "./Button";
+import activitiesMockData from "../../mocks/activitiesMock";
 
 describe("Given a Button component", () => {
   describe("When it is rendered with the text 'Modify", () => {
     test("Then it should show a button with the text 'MODIFY' in it", () => {
+      const mockData = activitiesMockData;
       customRenderProvider(
         <Button text="MODIFY" type="button" actionOnClick={() => {}} />,
+        mockData,
       );
 
       const expectedButtonText = "MODIFY";
@@ -18,6 +21,7 @@ describe("Given a Button component", () => {
     describe("When it is rendered with prop onClick", () => {
       test("Then it should call onClick method when it is clicked", () => {
         const testAction = vi.fn();
+        const mockData = activitiesMockData;
 
         customRenderProvider(
           <Button
@@ -26,6 +30,7 @@ describe("Given a Button component", () => {
             type={"button"}
             text={""}
           />,
+          mockData,
         );
 
         fireEvent.click(screen.getByRole("button"));
