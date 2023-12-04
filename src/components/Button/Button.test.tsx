5a50b7f3aa4238acd2e13a57b318e403353f7a3b
ramiro elisei <ramiroelisei@gmail.com>
2023-12-04 18:49:56 +0100

    Fix fireEvent in button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,42 +1,43 @@
-import { fireEvent, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import customRenderProvider from "../../utils/customRenderProvider";
 import Button from "./Button";
 import activitiesMockData from "../../mocks/activitiesMock";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Button component", () => {
+  const expectedButtonText = "MODIFY";
+
   describe("When it is rendered with the text 'Modify", () => {
     test("Then it should show a button with the text 'MODIFY' in it", () => {
       const mockData = activitiesMockData;
-      customRenderProvider(
-        <Button text="MODIFY" type="button" actionOnClick={() => {}} />,
-        mockData,
-      );
 
-      const expectedButtonText = "MODIFY";
-      const button = screen.getByRole("button");
+      customRenderProvider(<Button text="MODIFY" type="button" />, mockData);
+
+      const button = screen.getByRole("button", { name: expectedButtonText });
 
       expect(button).toHaveTextContent(expectedButtonText);
     });
+  });
 
-    describe("When it is rendered with prop onClick", () => {
-      test("Then it should call onClick method when it is clicked", () => {
-        const testAction = vi.fn();
-        const mockData = activitiesMockData;
+  describe("When it is rendered with prop onClick", () => {
+    test("Then it should call onClick method when it is clicked", async () => {
+      const testAction = vi.fn();
 
-        customRenderProvider(
-          <Button
-            actionOnClick={testAction}
-            disabled={false}
-            type={"button"}
-            text={""}
-          />,
-          mockData,
-        );
+      customRenderProvider(
+        <Button
+          actionOnClick={testAction}
+          disabled={false}
+          type="button"
+          text={expectedButtonText}
+        />,
+        activitiesMockData,
+      );
 
-        fireEvent.click(screen.getByRole("button"));
+      const button = screen.getByRole("button", { name: expectedButtonText });
 
-        expect(testAction).toHaveBeenCalledTimes(1);
-      });
+      await userEvent.click(button);
+
+      expect(testAction).toHaveBeenCalledTimes(1);
     });
   });
 });
