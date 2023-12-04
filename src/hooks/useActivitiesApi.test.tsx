f244aa07305c834ea9852a6bb4ddfa5d6bc72928
ramiro elisei <ramiroelisei@gmail.com>
2023-12-04 18:05:52 +0100

    Add provider wrapper to useActivities test

--- src/hooks/useActivitiesApi.test.tsx
+++ src/hooks/useActivitiesApi.test.tsx
@@ -1,17 +1,20 @@
 import { renderHook } from "@testing-library/react";
 import activitiesMockData from "../mocks/activitiesMock";
 import useActivitiesApi from "./useActivitiesApi";
+import { providerWrapper } from "../utils/providerWrapper";
 
 describe("Given a useActivitiesApi hook", () => {
   describe("When it gets the information of activities", () => {
-    test("Then it should show all the activities successfully", async () => {
+    test("Then it should return all the activities successfully", async () => {
       const expectedActivities = activitiesMockData;
 
       const {
         result: {
           current: { getActivities },
         },
-      } = renderHook(() => useActivitiesApi());
+      } = renderHook(() => useActivitiesApi(), {
+        wrapper: providerWrapper,
+      });
 
       const activities = await getActivities();
 
