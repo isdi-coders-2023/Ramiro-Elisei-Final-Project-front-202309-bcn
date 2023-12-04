79636a3ccc0da37892e2ef955db50831677d7b3c
ramiro elisei <ramiroelisei@gmail.com>
2023-12-04 19:44:33 +0100

    Fix card test

--- src/components/Button/ButtonStyle.ts
+++ src/components/Button/ButtonStyle.ts
@@ -4,23 +4,25 @@ const ButtonStyle = styled.button`
   border-radius: 50px;
   text-align: center;
 
-  &.button--small {
-    text-transform: uppercase;
-    font-size: 16px;
-    font-weight: 400;
-  }
+  .button {
+    &.button--small {
+      text-transform: uppercase;
+      font-size: 16px;
+      font-weight: 400;
+    }
 
-  &.button--solid {
-    background-color: ${({ theme }) => theme.color.main};
-    color: ${({ theme }) => theme.color.secondary};
-    padding: 12px 40px;
-  }
+    &.button--solid {
+      background-color: ${({ theme }) => theme.color.main};
+      color: ${({ theme }) => theme.color.secondary};
+      padding: 12px 40px;
+    }
 
-  &.button--dotted {
-    border: 2.5px dashed ${({ theme }) => theme.color.main};
-    background-color: ${({ theme }) => theme.color.secondary};
-    color: ${({ theme }) => theme.color.main};
-    padding: 9px 37px;
+    &.button--dotted {
+      border: 2.5px dashed ${({ theme }) => theme.color.main};
+      background-color: ${({ theme }) => theme.color.secondary};
+      color: ${({ theme }) => theme.color.main};
+      padding: 9px 37px;
+    }
   }
 `;
 
--- src/hooks/useActivitiesApi.tsx
+++ src/hooks/useActivitiesApi.tsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useCallback } from "react";
-import { ActivityStructure } from "../store/features/activities/types";
+import {
+  ActivitiesInitialStructure,
+  ActivityStructure,
+} from "../store/features/activities/types";
 import { useDispatch } from "react-redux";
 import { showLowdingActionCreator } from "../store/features/ui/uiSlice";
 
@@ -8,19 +11,20 @@ const useActivitiesApi = () => {
   const dispatch = useDispatch();
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-  const getActivities = useCallback(async () => {
-    try {
-      dispatch(showLowdingActionCreator());
+  const getActivities =
+    useCallback(async (): Promise<ActivitiesInitialStructure> => {
+      try {
+        dispatch(showLowdingActionCreator());
 
-      const { data: activities } = await axios.get<{
-        activities: ActivityStructure[];
-      }>("/activities");
+        const { data: activities } = await axios.get<{
+          activities: ActivityStructure[];
+        }>("/activities");
 
-      return activities;
-    } catch (error) {
-      throw (error as Error).message;
-    }
-  }, [dispatch]);
+        return activities;
+      } catch (error) {
+        throw (error as Error).message;
+      }
+    }, [dispatch]);
 
   return { getActivities };
 };
