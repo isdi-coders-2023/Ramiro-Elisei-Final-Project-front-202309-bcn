6019f50b584d5e00ad2e5b4f996806844b3bce92
ramiro elisei <ramiroelisei@gmail.com>
2023-12-04 18:04:03 +0100

    Add loading action creator to useActivities hook

--- src/hooks/useActivitiesApi.tsx
+++ src/hooks/useActivitiesApi.tsx
@@ -1,15 +1,26 @@
 import axios from "axios";
 import { useCallback } from "react";
 import { ActivityStructure } from "../store/features/activities/types";
+import { useDispatch } from "react-redux";
+import { showLowdingActionCreator } from "../store/features/ui/uiSlice";
 
 const useActivitiesApi = () => {
+  const dispatch = useDispatch();
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
   const getActivities = useCallback(async () => {
-    const { data: activities } = await axios.get<{
-      activities: ActivityStructure[];
-    }>("/activities");
-    return activities;
-  }, []);
+    try {
+      dispatch(showLowdingActionCreator());
+
+      const { data: activities } = await axios.get<{
+        activities: ActivityStructure[];
+      }>("/activities");
+
+      return activities;
+    } catch (error) {
+      throw (error as Error).message;
+    }
+  }, [dispatch]);
 
   return { getActivities };
 };
