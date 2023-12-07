f497c5b2afebfa313db2c4c3a45f9a3846a775ea
ramiro elisei <ramiroelisei@gmail.com>
2023-12-07 14:03:01 +0100

    Add get activities feedback test

--- src/pages/ActivitiesPage/ActivitiesPage.test.tsx
+++ src/pages/ActivitiesPage/ActivitiesPage.test.tsx
@@ -1,7 +1,9 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import customRenderProvider from "../../utils/customRenderProvider";
 import ActivitiesPage from "./ActivitiesPage";
 import activitiesMockData from "../../mocks/activitiesMock";
+import handlersError from "../../mocks/handlersError";
+import { server } from "../../mocks/node";
 
 describe("Given an ActivitiesPage", () => {
   describe("When the page is rendered", () => {
@@ -15,6 +17,20 @@ describe("Given an ActivitiesPage", () => {
       });
 
       expect(headingTitle).toBeInTheDocument();
+    });
+  });
+
+  describe("When an error occurs while loading activities once the Home page is rendered", () => {
+    test("Then it should show toastify notification message: 'I apologize, but we were unable to retrieve the activities.'", async () => {
+      const expectedTextError =
+        "I apologize, but we were unable to retrieve the activities.";
+      server.use(...handlersError);
+
+      customRenderProvider(<ActivitiesPage />, activitiesMockData);
+
+      await waitFor(() => {
+        expect(screen.getByText(expectedTextError)).toBeInTheDocument();
+      });
     });
   });
 });
