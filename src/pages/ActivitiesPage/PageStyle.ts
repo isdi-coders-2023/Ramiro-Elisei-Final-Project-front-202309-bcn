c8552af3bd82895c78f504c98755ce3cbec437b1
ramiro elisei <ramiroelisei@gmail.com>
2023-12-08 11:50:38 +0100

    Add form page styles

--- src/pages/ActivitiesPage/PageStyle.ts
+++ src/pages/ActivitiesPage/PageStyle.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const ActivitiesPageStyle = styled.div`
+export const PageStyle = styled.div`
   display: flex;
   flex-direction: column;
   text-align: left;
