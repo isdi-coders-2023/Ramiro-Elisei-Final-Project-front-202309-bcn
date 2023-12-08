fbd02165e04c76dac75cc7762c966a25d1815ac5
ramiro elisei <ramiroelisei@gmail.com>
2023-12-08 11:49:07 +0100

    Add activity form page

---
+++ src/pages/ActivitiesFormPage/ActivitiesFormPage.tsx
@@ -0,0 +1,13 @@
+import { PageStyle } from "../ActivitiesPage/PageStyle";
+
+const ActivitiesFormPage = (): React.ReactElement => {
+  return (
+    <>
+      <PageStyle>
+        <h1 className="page__title">Add activity</h1>
+      </PageStyle>
+    </>
+  );
+};
+
+export default ActivitiesFormPage;
