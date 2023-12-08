47f4582f087df063f4cd1a1240e03f0f0314665d
ramiro elisei <ramiroelisei@gmail.com>
2023-12-08 11:53:39 +0100

    Add activities form page route in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import ActivitiesPage from "../../pages/ActivitiesPage/ActivitiesPage";
 import Loading from "../Loading/Loading";
 import { useAppSelector } from "../../store/hooks";
 import ToastifyStyled from "../Toastify/ToastifyStyled";
+import ActivitiesFormPage from "../../pages/ActivitiesFormPage/ActivitiesFormPage";
 
 const App = (): React.ReactElement => {
   const isLoading = useAppSelector((state) => state.uiState.isLoading);
@@ -17,6 +18,8 @@ const App = (): React.ReactElement => {
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<ActivitiesPage />} />
+          <Route path="/" element={<Navigate to="/add" />} />
+          <Route path="/add" element={<ActivitiesFormPage />} />
         </Routes>
       </main>
     </>
